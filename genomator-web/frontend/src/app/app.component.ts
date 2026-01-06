b06f5af729602295f09bc8bbd837d5198b901d2b
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2026-01-06 15:43:06 +1030

    Refactor project dependencies and enhance UI accessibility; remove particles.js.Addded bootstrap.bundle.min.js to angular, add play/pause button for animation, and improve focus styles for buttons

--- genomator-web/frontend/src/app/app.component.ts
+++ genomator-web/frontend/src/app/app.component.ts
@@ -1,19 +1,54 @@
-import { Component, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 
 declare var particlesJS: any;
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
 })
-export class AppComponent implements OnInit {
-  public ngOnInit(): void {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('particles', { static: true }) particles: ElementRef | undefined;
+  faPlayCircle = faPlayCircle;
+
+  ngOnInit(): void {
     this.invokeParticles();
   }
 
-  public invokeParticles(): void {
-    particlesJS.load('particles-js', 'assets/particles.json', null);
+  ngAfterViewInit(): void {
+    const tooltipTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+    );
   }
+
+  invokeParticles(): void {
+    particlesJS.load('particles-js', 'assets/particles.json', () => {
+      console.log('Particles.js config loaded');
+    });
+  }
+
+  toggleAnimation(): void {
+    if (!this.particles) {
+      return;
+    }
+    const div = this.particles.nativeElement as HTMLDivElement;
+    if (div.style.display === 'none') {
+      div.style.display = 'block';
+    } else {
+      div.style.display = 'none';
+    }
+  }
+
   title = 'genomator-web';
 }
