ed7cb25304c02631c1bbaf2fe52800a914e6d95a
Brendan Hosking <brendan.hosking@gmail.com>
2026-02-18 15:45:14 +1100

    FEAT: Include false positives in privacy metric
    The privacy metric calculates the chance of someone's uniqe allele
    combination being reproduced in the synthetic dataset, but this does not
    fully describe the privacy of generated dataset. For example, a
    completely random dataset would have a high chance of reproducing
    someone's unique allele combination, but this is not informative as it
    likely contains most if not all possible 4-SNP allele combinations.
    
    To bring the privacy metric in line with the advantage an attacker would
    gain from looking for an individual's unique 4-allele combination in the
    generated dataset, subtract the false positive rate of such an attack
    before subtracting from 1 to make 0 undesirable and 1 desirable. The
    false positive rate is the frequency with which allele combinations that
    do not exist in the source dataset have at least one occurence in the
    synthetic dataset. The final privacy metric is then:
    1 - (
        P(combo in output | combo in input once)
        - P(combo in output | combo not in input)
    )

--- genomator-web/frontend/src/assets/privacy_metric.py
+++ genomator-web/frontend/src/assets/privacy_metric.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import Counter
+from functools import reduce
 import itertools
 import sys
 
@@ -27,24 +28,31 @@ def get_unique_reproduction_rate(input_records, generated_records, degree, trial
     assert (
         degree <= num_snps
     ), f"Cannot test {degree}-SNP combinations when VCF only contains {num_snps} SNPs"
+    total_absent = 0
+    total_absent_represented = 0
     total_uniques = 0
     total_reproduced = 0
     log(f"Sampling SNP combinations for unique {degree}-SNP combinations")
     for _ in tqdm(range(trials)):
         snp_indices = rng.choice(num_snps, degree, replace=False, shuffle=False)
-        input_sample_combinations = zip(*[input_records[i] for i in snp_indices])
-        generated_sample_combinations = zip(
-            *[generated_records[i] for i in snp_indices]
+        input_sample_combinations = list(zip(*[input_records[i] for i in snp_indices]))
+        generated_combinations_set = set(
+            zip(*[generated_records[i] for i in snp_indices])
         )
         input_uniques, reproduced_uniques = count_uniques_from_a_in_b(
-            input_sample_combinations, generated_sample_combinations
+            input_sample_combinations, generated_combinations_set
         )
+        absent, absent_represented = count_absent_from_a_in_b(
+            input_sample_combinations, generated_combinations_set
+        )
+        total_absent += absent
+        total_absent_represented += absent_represented
         total_uniques += input_uniques
         total_reproduced += reproduced_uniques
     if total_uniques == 0:
         log("Input data contains no unique {degree}-SNP combinations")
-        return 0
-    return total_reproduced / total_uniques
+        return 0, 0
+    return total_reproduced / total_uniques, total_absent_represented / total_absent
 
 
 def parse_vcf_to_record_strings(vcf_file):
@@ -75,11 +83,22 @@ def load_file(filename):
 
 
 # count how many unique SNP combinations from a appear in b
-def count_uniques_from_a_in_b(a, b):
+def count_uniques_from_a_in_b(a, set_b):
     uniques = set(combo for combo, count in Counter(a).items() if count == 1)
     if not uniques:
         return 0, 0
-    return len(uniques), len(uniques & set(b))
+    return len(uniques), len(uniques & set_b)
+
+
+def multiply_size(previous, collection):
+    return previous * len(collection)
+
+
+def count_absent_from_a_in_b(a, set_b):
+    all_possible = reduce(multiply_size, (set(alleles) for alleles in zip(*a)), 1)
+    set_a = set(a)
+    absent = all_possible - len(set_a)
+    return absent, len(set_b - set_a)
 
 
 def score_privacy(
@@ -87,10 +106,10 @@ def score_privacy(
 ) -> float:
     input_records = load_file(input_vcf_file)
     generated_records = load_file(generated_vcf_file)
-    unique_reproduction_rate = get_unique_reproduction_rate(
+    unique_reproduction_rate, absent_represented_rate = get_unique_reproduction_rate(
         input_records, generated_records, degree, trials
     )
-    return 1 - unique_reproduction_rate
+    return 1 - (unique_reproduction_rate - absent_represented_rate)
 
 
 @click.command()
